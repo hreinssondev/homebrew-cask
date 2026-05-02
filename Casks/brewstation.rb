e5793d8e6b4004e67e6491ca32f85c63ede5ddfd
hreinssondev <hreinssondev@gmail.com>
2026-05-02 21:52:22 +0200

    Revert BrewStation to 1.3

--- Casks/brewstation.rb
+++ Casks/brewstation.rb
@@ -1,8 +1,8 @@
 cask "brewstation" do
-  version "1.4"
-  sha256 "738691b9b9b170b38d0c074e891c7f92a536821bfdcc72337995a3d61a143287"
+  version "1.3"
+  sha256 "1c1b1d698df59218475170638bfe7d98b5c226ffcae148ce4955871c1dfb9df6"
 
-  url "https://github.com/hreinssondev/brewstation/releases/download/#{version}/Brewstation.dmg"
+  url "https://github.com/hreinssondev/brewstation/releases/download/#{version}/BrewStation.dmg"
   name "BrewStation"
   desc "Homebrew GUI manager with snapshot saving and restoring"
   homepage "https://github.com/hreinssondev/brewstation"
